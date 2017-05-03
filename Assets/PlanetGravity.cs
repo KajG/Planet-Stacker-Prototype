d70b792538bfd0e44020634751dd2c38a2b96397
KajG <kaaiman99@hotmail.com>
2017-05-03 19:09:36 +0200

    Initial commit

---
+++ Assets/PlanetGravity.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetGravity : MonoBehaviour {
+	[SerializeField]private float _gravityDistance;
+	[SerializeField]private float _forceAmount;
+	[SerializeField]private GameObject _planetGizmo;
+	public float getGravityDistance{get{return _gravityDistance;}}
+	public float getForceAmount{get{return _forceAmount;}}
+	void OnDrawGizmos(){
+		Gizmos.color = Color.white;
+		Gizmos.DrawSphere (_planetGizmo.transform.position, _gravityDistance);
+	}
+}
---
+++ Assets/ProjectileBehaviour.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileBehaviour : MonoBehaviour {
+	private GameObject _planet;
+	private float _distance;
+	private Rigidbody2D _rig;
+	private float _gravityDistance;
+	private float _forceAmount;
+	private bool hit;
+	[SerializeField]private float timer;
+	[SerializeField]private float speed;
+	void Start () {
+		_planet = GameObject.Find ("Planet");
+		_rig = GetComponent<Rigidbody2D> ();
+		_gravityDistance = _planet.GetComponent<PlanetGravity> ().getGravityDistance;
+		_forceAmount = _planet.GetComponent<PlanetGravity> ().getForceAmount;
+	}
+
+	void Update () {
+		if (!hit) {
+			_distance = Vector3.Distance (_planet.transform.position, transform.position);
+			if (_distance <= _gravityDistance) {
+				timer += Time.fixedDeltaTime; 
+				print (timer);
+				_forceAmount += timer;
+				Vector3 _desiredDirection = _planet.transform.position - transform.position;
+				_desiredDirection.Normalize ();
+				_rig.AddForce (new Vector2 (_desiredDirection.x, _desiredDirection.y) * _forceAmount * (_distance / 5));
+			}
+			if (Input.GetKey (KeyCode.Space)) {
+				_rig.AddForce (Vector2.up * speed);
+			}
+		}
+	}
+	void OnCollisionEnter2D(Collision2D other){
+		hit = true;	
+		_rig.velocity = new Vector2(0,0);
+	}
+}
