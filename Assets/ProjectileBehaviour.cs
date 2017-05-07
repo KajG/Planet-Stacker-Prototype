7ab82c1189ac3f21ae113abb84c4ab3c6b091acd
Kaj <kaaiman99@hotmail.com>
2017-05-07 12:30:05 +0200

    Added prototype cannon

--- Assets/ProjectileBehaviour.cs
+++ Assets/ProjectileBehaviour.cs
@@ -4,17 +4,20 @@ using UnityEngine;
 
 public class ProjectileBehaviour : MonoBehaviour {
 	private GameObject _planet;
+	private Transform _cannonPos;
 	private Rigidbody2D _rig;
 	private ParticleEffectProjectile _particleEffect;
 	private float _distance;
 	private float _gravityDistance;
 	private float _forceAmount;
 	private bool hit;
+	private bool gotShot;
 	[SerializeField]private float timer;
 	[SerializeField]private float speed;
 	[SerializeField]private float _forceGroundTime;
 	[SerializeField]private int _spawnAmount;
 	void Start () {
+		_cannonPos = GameObject.Find ("Cannon").GetComponent<Transform> ();
 		_planet = GameObject.Find ("Planet");
 		_rig = GetComponent<Rigidbody2D> ();
 		_particleEffect = GameObject.Find ("ParticlePlanetHit").GetComponent<ParticleEffectProjectile> ();
@@ -26,7 +29,7 @@ public class ProjectileBehaviour : MonoBehaviour {
 		float AngleRad = Mathf.Atan2(_planet.transform.position.y - transform.position.y, _planet.transform.position.x - transform.position.x);
 		float AngleDeg = (180 / Mathf.PI) * AngleRad;
 		this.transform.rotation = Quaternion.Euler(0, 0, AngleDeg);
-		if (!hit) {
+		if (!hit && gotShot) {
 			_distance = Vector3.Distance (_planet.transform.position, transform.position);
 			if (_distance <= _gravityDistance) {
 				timer += Time.fixedDeltaTime / _forceGroundTime; 
@@ -36,9 +39,9 @@ public class ProjectileBehaviour : MonoBehaviour {
 				_desiredDirection.Normalize ();
 				_rig.AddForce (new Vector2 (_desiredDirection.x, _desiredDirection.y) * _forceAmount * (_distance / 5));
 			}
-			if (Input.GetKey (KeyCode.Space)) {
-				_rig.AddForce (Vector2.up * speed);
-			}
+		} else if(!gotShot){
+			transform.position = _cannonPos.position;
+			transform.rotation = _cannonPos.rotation;
 		}
 	}
 	void OnCollisionEnter2D(Collision2D other){
@@ -51,7 +54,12 @@ public class ProjectileBehaviour : MonoBehaviour {
 	}
 	void SpawnEntity(){
 		for (int i = 1; i < _spawnAmount + 1; i++) {
-			Instantiate (gameObject, transform.position + transform.right * -i, transform.rotation);
+			GameObject obj = Instantiate (gameObject, transform.position + transform.right * -i, transform.rotation);
+			Destroy (obj.GetComponent<ProjectileBehaviour> ());
 		}
+	}
+	public void Shoot(float _shotSpeed){
+		gotShot = true;
+		_rig.AddForce (transform.up * _shotSpeed);
 	}
 }
---
+++ Assets/ShootingBehaviour.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootingBehaviour : MonoBehaviour {
+
+	private Vector3 _rot;
+	private float _angle;
+	private ProjectileBehaviour Projectilebehaviour;
+	[SerializeField]private float _rotSpeed;
+	[SerializeField]private float _shootSpeed;
+	void Start () {
+		Projectilebehaviour = GameObject.Find ("Test block").GetComponent<ProjectileBehaviour> ();
+	}
+	
+	void Update () {
+		_rot = new Vector3 (0, 0, _angle);
+		transform.localEulerAngles = _rot;
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			_angle += _rotSpeed * Time.fixedDeltaTime;
+		}
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			_angle -= _rotSpeed * Time.fixedDeltaTime;
+		}
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			Projectilebehaviour.Shoot (_shootSpeed);
+		}
+	}
+}
