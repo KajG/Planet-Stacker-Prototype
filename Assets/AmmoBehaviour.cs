2571a455a01f553d0924f2930ecbb07ff32a1f68
Kaj <kaaiman99@hotmail.com>
2017-05-07 15:43:32 +0200

    Begin van ammo

---
+++ Assets/AmmoBehaviour.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmmoBehaviour : MonoBehaviour {
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+}
